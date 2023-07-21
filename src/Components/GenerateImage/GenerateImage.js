d7ea3ffe60a2f4653feb5bca0f4169490d167b23
JHPark02 <applestrawberryg@gmail.com>
2023-07-21 13:56:32 +0900

    feature: Add feature that block access to image-generating page
    when user is not logged in.

--- src/Components/GenerateImage/GenerateImage.js
+++ src/Components/GenerateImage/GenerateImage.js
@@ -1,4 +1,16 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginContext } from "../../LoginContextProvider";
+
 const GenerateImage = () => {
+    const { isLogin } = useContext(LoginContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isLogin)
+            navigate("/SignIn")
+    })
+
     return (
         <div>
             GenerateImage Page
